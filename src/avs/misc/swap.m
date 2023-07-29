3eb0d47c4c9389ac3456cec0902480247a9b89aa
Andrei Smoliakov <andrei.smoliakov@tuni.fi>
2023-07-29 03:15:24 +0300

    Add swap function into misc

---
+++ src/avs/misc/swap.m
@@ -0,0 +1,5 @@
+function [b, a] = swap(a, b)
+% SWAP Swap two arguments
+%
+%    [B, A] = SWAP(A, B) returnes the first input argument as second output
+%    argument and vice versa. Performs no copying.
