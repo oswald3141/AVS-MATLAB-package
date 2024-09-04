f5d371835f50cbae7a8bc3731d10b061e17c25dc
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2024-09-04 19:24:14 +0300

    Add option allowing scalars in mustHaveSameSize

--- src/avs/validators/mustHaveSameSize.m
+++ src/avs/validators/mustHaveSameSize.m
@@ -1,15 +1,36 @@
-function mustHaveSameSize(A, B, allowEmpty)
+function mustHaveSameSize(A, B, varargin)
 % MUSTHAVESAMESIZE Validate that the variables have the same size
 %
 %   MUSTHAVESAMESIZE(A, B) throws an exception if A has a different size
 %   than B.
 %
 %   MUSTHAVESAMESIZE(A, B, "allow-all-empties") does not throw an exception
-%   if A is emtpy.
+%   if A is emtpy. Can be useful if A is an ignorable parameter.
+%
+%   MUSTHAVESAMESIZE(A, B, "allow-scalar") does not throw an exception
+%   if A is scalar. Can be useful since a scalar can be shaped into any
+%   size by repetition.
+%
+%   MUSTHAVESAMESIZE(A, B, "allow-all-empties", "allow-scalar") a
+%   combination of the two previous options.
 
 if nargin > 2
-    if strcmp(allowEmpty, "allow-all-empties") && isempty(A)
-        return;
+    for i = 1:length(varargin)
+        if strcmp(varargin{i}, "allow-all-empties")
+            if isempty(A)
+                return;
+            end
+        elseif strcmp(varargin{i}, "allow-scalar")
+            if isscalar(A)
+                return;
+            end
+        else
+            throwAsCaller(MException( ...
+                "mustHaveSameSize:invalidComparisonParameter", ...
+                "The value of the comparison strictness " + ...
+                    "argument is not recognized." ...
+            ));
+        end
     end
 end
 
