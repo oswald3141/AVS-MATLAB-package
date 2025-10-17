f6e3bf919c891ae9690a2281425377d7dca97f75
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2025-10-17 17:34:01 +0300

    Add mustBeEven and mustBeOdd validators

---
+++ src/avs/validators/mustBeEven.m
@@ -0,0 +1,27 @@
+function mustBeEven(A)
+% MUSTBEEVEN Validate that value is even
+%
+%   MUSTBEEVEN(A) throws an error is A contains odd numbers.
+%
+%   See also: MUSTBEODD, MUSTBEEVENPOWEROFTWO, MUSTBENATURALPOWEROFTWO
+
+if ~(isnumeric(A) || islogical(A))
+    throwAsCaller(MException( ...
+        "mustBeEven:nonNumericOrLogicalInput", ...
+        "The input must be numeric or logical."));
+end
+
+if ~isreal(A)
+    throwAsCaller(MException( ...
+        "mustBeEven:nonRealInput", ...
+        "The input must be real."));
+end
+
+if ~all(mod(A,2) == 0, "all")
+    throwAsCaller(MException( ...
+        "mustBeEven:notEven", ...
+        "The array must contain only even numbers." ...
+    ));
+end
+
+end
---
+++ src/avs/validators/mustBeOdd.m
@@ -0,0 +1,27 @@
+function mustBeOdd(A)
+% MUSTBEODD Validate that value is odd
+%
+%   MUSTBEODD(A) throws an error is A contains even numbers.
+%
+%   See also: MUSTBEEVEN, MUSTBEEVENPOWEROFTWO, MUSTBENATURALPOWEROFTWO
+
+if ~(isnumeric(A) || islogical(A))
+    throwAsCaller(MException( ...
+        "mustBeOdd:nonNumericOrLogicalInput", ...
+        "The input must be numeric or logical."));
+end
+
+if ~isreal(A)
+    throwAsCaller(MException( ...
+        "mustBeOdd:nonRealInput", ...
+        "The input must be real."));
+end
+
+if ~all(mod(A,2) == 1, "all")
+    throwAsCaller(MException( ...
+        "mustBeOdd:notOdd", ...
+        "The array must contain only odd numbers." ...
+    ));
+end
+
+end
