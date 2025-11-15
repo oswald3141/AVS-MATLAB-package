547921a2a84df3247d836d2687fe593eab27a178
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2025-11-16 01:43:00 +0200

    Fix ne and eq in Signal

--- src/avs/signal_analysis/@Signal/eq.m
+++ src/avs/signal_analysis/@Signal/eq.m
@@ -1,16 +1,20 @@
 function r = eq(a, b)
-% EQ Checks if two signals are equal
+% EQ Checks if signals are equal
 %
-%    R = NE(A, B) compares samples and sample rates of A and B, returns
-%    True if they match. Disregards descriptions.
-%    This call also represents overloaded "equality" operator, so it can
-%    be performed as
-%       R = A == B;
+%   R = EQ(A, B) compares samples and sample rates of A and B disregarding
+%   the descriptions.
 
-assert(isa(a, "Signal") && isa(b, "Signal"), ...
-    "Signal:comparisonNotDefined", ...
-    "Comparison is supported only for to objects of Signal.");
+if ~isa(a, "Signal") || ~isa(b, "Signal")
+    error("AVS:Signal:ComparisonNotDefined", ...
+        "Comparison between %s and %s is not supported.", ...
+        class(a), class(b));
+end
 
-r = isequal(a.samples, b.samples) && (a.Fs == b.Fs);
+r = Signal.bsxfun(@f1, a, b, false);
+
+    function r = f1(a, b)
+        r = unsafe_almeq(a.Fs, b.Fs) && ...
+            isequal(a.samples, b.samples);
+    end
 
 end
