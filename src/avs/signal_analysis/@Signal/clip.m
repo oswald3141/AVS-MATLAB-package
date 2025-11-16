d96331d08097f2f98909dee071a08acf22d48fe3
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2025-11-16 21:08:24 +0200

    Refactor clip in Signal

--- src/avs/signal_analysis/@Signal/clip.m
+++ src/avs/signal_analysis/@Signal/clip.m
@@ -1,34 +1,56 @@
-function this = clip(this, component, maxValue, minValue)
+function this = clip(this, type, maxValue, minValue)
 % CLIP Clips samples to the specified boundries
 %
-%   S = CLIP(S, COMPONENT, MAXVALUE, MINVALUE) limits the real (if
-%   COMPONENT is set to "re") or imaginary (if COMPONENT is set to "im")
-%   part of the samples to the [minValue; maxValue] range. Returnes the
-%   alteres copy of S.
+%   THIS = CLIP(THIS, TYPE, MAXVALUE, MINVALUE) limits the real part (if
+%   TYPE is set to "re"), imaginary part (if TYPE is set to "im"), both of
+%   them (if TYPE is set to "reim", or the absolute value (if TYPE is set
+%   to "abs") of the samples to the [minValue; maxValue] range.
+%   MAXVALUE must be greater that or equal to MINVALUE.
 
 arguments(Input)
     this (1,1) Signal
-    component {mustBeMember(component, ["re" "im"])}
+    type {mustBeMember(type, ["re" "im", "reim", "abs"])}
     maxValue (1,1) {mustBeReal} = Inf;
-    minValue (1,1) {mustBeReal} = -Inf;
+    minValue (1,1) {mustBeReal, ...
+        mustBeLessThanOrEqual(minValue, maxValue)} = -Inf;
 
 end
 
 if this.isComplex
-    if component == "re"
-        this.samples = clip_real_array(this.re, maxValue, minValue) + ...
-            1j*this.im;
-    elseif component == "im"
-        this.samples = this.re + ...
-            1j*clip_real_array(this.im, maxValue, minValue);
+    if type == "re"
+        this.samples = ...
+            clip_real_array(real(this.samples), maxValue, minValue) + ...
+            1j*imag(this.samples);
+    elseif type == "im"
+        this.samples = ...
+            real(this.samples) + ...
+            1j*clip_real_array(imag(this.samples), maxValue, minValue);
+    elseif type == "reim"
+        this.samples = ...
+            clip_real_array(real(this.samples), maxValue, minValue) + ...
+            1j*clip_real_array(imag(this.samples), maxValue, minValue);
+    elseif type == "abs"
+        if maxValue >= 0
+            this.samples = ...
+                clip_real_array( ...
+                    abs(this.samples), maxValue, minValue).* ...
+                exp(1j*angle(this.samples));
+        end
     else
         assert(0);
     end
 else
-    if component == "im"
+    if type == "im"
         return;
-    else
+    elseif type == "abs"
+        if maxValue >= 0
+            this.samples = ...
+                clip_real_array(this.samples, maxValue, -maxValue);
+        end
+    elseif type == "re" || type == "reim"
         this.samples = clip_real_array(this.samples, maxValue, minValue);
+    else
+        assert(0);
     end
 end
 
