f6dff4181f43e2526105313a2d708e0a79429f70
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2025-11-16 02:30:36 +0200

    Remove unsafe_almeq from check_arith_bin_op_compatibility in Signal

--- src/avs/signal_analysis/@Signal/private/check_arith_bin_op_compatibility.m
+++ src/avs/signal_analysis/@Signal/private/check_arith_bin_op_compatibility.m
@@ -20,16 +20,4 @@ if ~unsafe_almeq(Fs1, Fs2)
         "Signals must have same sample rate, but %f~=%f.", Fs1, Fs2));
 end
 
-    function r = unsafe_almeq(a, b)
-        REALMAX = (2-2^(-52))*2^1023; % realmax
-        REALMIN = 2^(-1022); % realmin
-        EPSx128 = 2^-45; % 128*eps()
-
-        abs_th = REALMIN;
-        epsilon = EPSx128;
-        diff = abs(a - b);
-        norm = min(abs(a+b), REALMAX);
-        r = diff < max(abs_th, epsilon*norm);
-    end
-
 end
