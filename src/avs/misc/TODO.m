51bb977a5aca31cb75f88c069ae1b0bb2337c7f3
Andrei Smoliakov (TAU) <andrei.smoliakov@tuni.fi>
2026-02-18 16:54:31 +0200

    Add `unreachable` and `TODO` functions to mark corresponding code

---
+++ src/avs/misc/TODO.m
@@ -0,0 +1,38 @@
+function TODO(addmsg, severity)
+% TODO Throw an error if there is no implementation
+%
+%   TODO() marks a place that is not yet implemented. If the place is
+%   reached, TODO throws an error with an explanatory message.
+%
+%   TODO(STR) adds STR to the message of the thrown error. If STR is empty,
+%   it is ignored and the behaviour is the same as of TODO().
+%
+%   TODO(STR, SEVERITY) throws an error if SEVERITY is "error", and
+%   displays a warning if it is "warning".
+%
+%   See also: UNREACHABLE
+
+
+arguments
+    addmsg {mustBeTextScalar}
+    severity {mustBeMember(severity, ["error" "warning"])} = "error";
+end
+
+id = 'AVS:TODO';
+
+if addmsg == ""
+    msg = 'The implementation is not finished.';
+else
+    msg = ['The implementation is not finished: ' ...
+        convertStringsToChars(addmsg)];
+end
+
+if severity == "error"
+    throwAsCaller(MException(id, msg));
+elseif severity == "warning"
+    warning(id, msg);
+else
+    unreachable();
+end
+
+end
---
+++ src/avs/misc/unreachable.m
@@ -0,0 +1,13 @@
+function unreachable()
+% UNREACHABLE Throw an error if line reached
+%
+%   UNREACHABLE() marks a line that is supposed to be never reached, e.g.
+%   otherwise clause of a swith-case. If the line is reached, the function
+%   throws an exception.
+%
+%   See also: TODO
+
+throwAsCaller(MException('AVS:unreachable', ...
+    'This line is supposed to be unreachable.'));
+
+end
